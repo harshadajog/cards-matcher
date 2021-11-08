0f10038c37c0924373cbaac59cc82f976ef7caf0
Netapp DPG <netappdpg@Netapps-Air.lan>
2021-11-07 19:03:14 -0800

    added cards

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Add flip to each card.
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+  this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
